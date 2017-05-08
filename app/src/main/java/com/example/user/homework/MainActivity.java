25c97e93eb8d4c34ac71e2b222bc6d23c5f4b5de
a839024 <a839024@gmail.com>
2017-05-08 23:48:12 +0800

    Add fake data for listview

--- app/src/main/java/com/example/user/homework/MainActivity.java
+++ app/src/main/java/com/example/user/homework/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.user.homework;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnAdd;
+    private ListView lvNote;
+    private ArrayList<String> item;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnAdd = (Button)findViewById(R.id.btnAdd);
+        lvNote = (ListView)findViewById(R.id.lvNote);
+        item = new ArrayList<String>();
+        item.add("a");
+        item.add("b");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, item);
+        lvNote.setAdapter(arrayAdapter);
     }
 }
